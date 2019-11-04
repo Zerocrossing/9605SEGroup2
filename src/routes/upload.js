0a398f53735b352684477e8bfff0d9fb28c76688
Rob B <infamousrob@gmail.com>
2019-11-04 11:28:48 -0330

    fix for upload.js

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -16,59 +16,38 @@ router.post('/', function (req, res) {
         return res.status(400).send('No files were uploaded.');
     }
     //region Validation
-    let rawFileNames =[]
+    let rawFileNames = []
     let retVal;
     let csvFileNo = 0;
-    let files = req.files;
+    let files = req.files.uploaded;
     //csv and Transmission should be in config
     // just one csv file is allowed, all the others should be rawfile
-    for (let i = 0; i < files.length; i++){
+    for (let i = 0; i < files.length; i++) {
         let splittedName = files[i].name.split(".");
-        var ext = splittedName[splittedName.length-1];
+        var ext = splittedName[splittedName.length - 1];
 
-        if(ext === 'csv'){
+        if (ext === 'csv') {
 
             csvFileNo++;
-            if(csvFileNo>1)
-            {
+            if (csvFileNo > 1) {
                 console.log("You are allowed to submit just one metadata file in each upload!");
                 //Stop and show error message to the user
             }
-            let content  = files[i].data.toString();
+            let content = files[i].data.toString();
             retVal = validator.csvJSON(content);
 
 
-        } else if (ext ==='Transmission')
-        {
-            rawFileNames.push(splittedName[0]+splittedName[1])
+        } else if (ext === 'Transmission') {
+            rawFileNames.push(splittedName[0] + splittedName[1])
         }
-        else
-        {
+        else {
             console.log("There are invalid type of file in the submission!")
             //Stop and show error message to the user
         }
     }
 
-    let isMatch = validator.matchRawFilesAndMetadataFiles(rawFileNames,retVal.fileNames)
+    let isMatch = validator.matchRawFilesAndMetadataFiles(rawFileNames, retVal.fileNames)
     let emptyMandatoryFields = validator.checkMandatoryFields(retVal.json)
-    //endregion validation
-
-    //DEBUG display the names of the files and make a fake request to the DB
-    // var out = '';
-    // // multiple files are stored in an array
-    // if (Array.isArray(req.files.uploaded)) {
-    //     for (var file in req.files.uploaded) {
-    //         var fileObj = req.files.uploaded[file];
-    //         db.post(fileObj);
-    //         out += fileObj.name + '<br>';
-    //     }
-    // //single files an object
-    // } else {
-    //     db.post(req.files.uploaded);
-    //     out += req.files.uploaded.name;
-    // }
-    // res.send('You uploaded these files:<br>' + out);
-
 });
 
 module.exports = router;
