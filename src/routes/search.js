81f2e16688972a0e174b3a6f8c379d032ff0ae04
Rob B <r.bishop@mun.ca>
2019-11-07 12:23:41 -0330

    updated search page with basic functionality and proper terms currently doesn't perform a query, just displays everything in the DB

--- src/routes/search.js
+++ src/routes/search.js
@@ -2,20 +2,21 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../Auxiliaries/database');
+const config = require('../config.json');
 
 // Search page render
 router.get('/', function (req, res) {
-    res.render('search', {title: 'Nature\'s Palette'});
+    res.render('search', {title: 'Nature\'s Palette', searchTerms: config.searchTerms});
 });
 
 // Post request: responds to search query
 router.post('/', async function (req, res) {
     let query = req.body;
     let results = await db.getQueryResults(query);
-    console.log(results);
     res.render('search',
         {
             title: 'Nature\'s Palette',
+            searchTerms: config.searchTerms,
             results: results
         }
     )
