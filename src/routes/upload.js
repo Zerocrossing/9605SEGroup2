716a711b3fb6c20a9d0f429a646dd1a37f463bba
sds660 <sds660@mun.ca>
2019-11-18 09:13:31 -0330

    Accidentally commented out db save

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -20,7 +20,7 @@ router.post('/', function (req, res) {
     let validationStatus = validator.validateSubmission(req)
     if (validationStatus.isValid) {
         res.render('upload', {title: 'Nature\'s Palette', msg: 'Files successfully uploaded!'});
-        //db.saveData(validationStatus.json, req.files.raw)
+        db.saveData(validationStatus.json, req.files.raw)
     } else {
         res.render('upload', {title: 'Nature\'s Palette', msg: 'There was an error uploading the files.\n' + validationStatus.message});
     }
