d43b62c53099cac7702f1d487c11fc252c8409b2
Rob B <r.bishop@mun.ca>
2019-11-25 17:09:39 -0330

    Download works if path exists

--- src/routes/download.js
+++ src/routes/download.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 const querystring = require('querystring');
 var db = require('../Auxiliaries/database');
+const admzip = require('adm-zip');
 
 
 /* GET about listing. */
@@ -10,15 +11,22 @@ router.get('/', async function (req, res) {
     // get query (identical to search query) and find filepaths
     let query = querystring.decode(req.query.download);
     //todo this returns nothing currently
-    let filePaths = await db.getPathsFromQuery(query);
-    res.render('download', {title: 'Nature\'s Palette'});
+    let filesZip = new admzip();
+    let filePaths = await db.getPathsFromQuery(query, filesZip);
+    addFilesToZip(filePaths, filesZip);
+    let sendMe = await filesZip.toBuffer();
+    //todo how to rename this file?
+    res.contentType('zip');
+    res.send(sendMe);
 });
 
 module.exports = router;
 
 //upon being passed the result of a mongoquery this returns a zip containing all the local files
-function getZipFromFilePaths(queryResult) {
-    for (const path of queryResult){
-        //todo
+function addFilesToZip(queryResult, zip) {
+    for (const result of queryResult) {
+        let path = result["filePath"];
+        zip.addLocalFile(path);
     }
+    return zip;
 }
